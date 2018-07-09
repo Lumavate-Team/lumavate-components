1ad9e8395c16b7adbc4fdd65758095f1a32ee809
Matt Kelsey <m.kelsey@lumavate.com>
2018-07-09 14:04:23 -0400

    Lumavate Header

--- src/components/lumavate-header/lumavate-header.tsx
+++ src/components/lumavate-header/lumavate-header.tsx
@@ -1,8 +1,9 @@
-import { Component, Prop, Method } from '@stencil/core';
+import { Component, Prop, Method, Element } from '@stencil/core';
 
 @Component({
   tag: 'lumavate-header',
-  styleUrl: 'lumavate-header.scss'
+  styleUrl: 'lumavate-header.scss',
+  shadow:true
 })
 
 export class LumavateShareText {
@@ -10,14 +11,15 @@ export class LumavateShareText {
   @Prop() FontColor: string = '#F1F1F1';
   @Prop() BackgroundColor: string = '#252525';
   @Prop() ShowBackButton: boolean = true;
-  @Prop() Text: string = 'placeholder text';
+  @Prop() Text: string = '';
+
+  @Element() el: HTMLElement;
 
   @Method()
   showBackButton() {
-    let backButton: any = document.getElementsByClassName('back-button')
-
+    let backButton: any = this.el.shadowRoot.querySelector('.back-button')
     if (!this.ShowBackButton) {
-      backButton[0].style.display = 'none'
+      backButton.style.display = 'none'
     }
   }
 
@@ -33,12 +35,12 @@ export class LumavateShareText {
           @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
         </style>
         <div class="nav-wrapper" style={{ "color": this.FontColor }}>
-          <div id='back-button'>
+          <div class='back-button'>
             <a href="javascript:history.back()" style={{ "color": this.FontColor }}>
               <i class="material-icons">keyboard_arrow_left</i>
             </a>
           </div>
-          <div id="text">
+          <div class="text">
             {this.Text}
           </div>
           <div class="forward-button"></div>
