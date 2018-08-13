986bc0a3a7765d83084495528ebb87982c9cb5a5
Andrew Jackson <andrew.c.j1995@gmail.com>
2018-08-13 16:17:06 -0400

    Add in swiping between images and remove pinch and zoom
    Pinch and zoom was removed due to difficulties getting the originial
    libary to work with swiping . The css properties needed for that pinch
    and zoom library conflicted with swiping. Alternative pinch and zoom
    solultions are being looked into.

--- src/components/lumavate-carousel/lumavate-carousel.tsx
+++ src/components/lumavate-carousel/lumavate-carousel.tsx
@@ -1,5 +1,5 @@
-import { Component, Prop, Event, EventEmitter, Element, Method, Listen } from '@stencil/core';
-import PinchZoom from 'pinch-zoom-js'
+import { Component, Prop, Event, EventEmitter, Element, Method } from '@stencil/core';
+import HammerJS from 'hammerjs'
 
 
 @Component({
@@ -17,148 +17,143 @@ export class LumavateCarosel {
   @Prop() holdToZoom: boolean = false;
 
   images: Array<any>
-  carouselIndex: number = 1
   pinchEL: any
-  pz: any
+  swipeEL: HTMLElement
+  swipeElLbox: HTMLElement
+  swipeManager: any
+  swipeManagerLbox: any
   zoomed: boolean = false
+
+  slidePanelSelector = '.slider-panel';
+  sensitivity = 18 // horizontal % needed to trigger swipe
+  activeSlide = 0;
+  slideCount = 0;
+  timer: any
 
   componentWillLoad() {
     this.images = JSON.parse(this.CarouselImages)
+    this.slideCount = this.images.length
   }
 
   componentDidLoad() {
-
-    if(this.pinchZoom){
-      this.initPinchZoom()
-    } else{
-      let modal: any = document.getElementsByClassName('modal')
-      let closeButton: any = document.getElementsByClassName('close')
-      modal[0].style.display = 'none'
-      closeButton[0].style.display = 'none'
-    }
-
-    this.showSlides(this.carouselIndex)
-    this.setArrowColor()
-  }
-
-  @Method()
-  initPinchZoom() {
-    this.pinchEL = document.querySelector('div.pinch-zoom');
-    if(this.holdToZoom){
-      this.pz = new PinchZoom(this.pinchEL, { zoomOutFactor: 9, maxZoom: 2 });
-    } else{
-      this.pz = new PinchZoom(this.pinchEL, {zoomOutFactor: 1.3});
-    }
-
+    this.initSwipe()
     let modal: any = document.getElementsByClassName('modal')
-    let slides: any = document.getElementsByClassName("pinch-zoom-container")
+    let container:any = document.getElementsByClassName('container')
+    let wrapper: any = document.getElementsByClassName('wrapper')
     let closeButton: any = document.getElementsByClassName('close')
     modal[0].style.display = 'none'
-    slides[0].style.position = ''
+    container[0].style.width = 100 * this.slideCount + '%'
+    modal[0].style.width = 100 * this.slideCount + '%'
+    wrapper[0].style.display = 'none'
     closeButton[0].style.display = 'none'
   }
 
   @Method()
-  setArrowColor() {
-    let nextArrow: any = document.getElementsByClassName("next")
-    let previousArrow: any = document.getElementsByClassName("previous")
+  initSwipe() {
+    this.swipeEL = document.querySelector('.container')
+    this.swipeElLbox = document.querySelector('.modal')
+    this.swipeManager = new HammerJS.Manager(this.swipeEL)
+    this.swipeManagerLbox = new HammerJS.Manager(this.swipeElLbox)
+    this.swipeManager.add(new HammerJS.Pan({ threshold: 0, pointers: 0 }))
+    this.swipeManagerLbox.add(new HammerJS.Pan({ threshold: 0, pointers: 0 }))
+    this.swipeManager.on('pan', (e) => {
+      //Calculate pixel movements into 1:1 screen percents so gestures track with motion
+      let percentage = 100 / this.slideCount * e.deltaX / window.innerWidth
+      //Multiply percent by # of slide we’re on
+      let percentageCalculated = percentage - 100 / this.slideCount * this.activeSlide
+      this.swipeEL.style.transform = 'translateX( ' + percentageCalculated + '% )'
+      this.determineValidSwipe(e, percentage)
+    })
 
-    nextArrow[0].style.color = this.arrowColor
-    previousArrow[0].style.color = this.arrowColor
+    this.swipeManagerLbox.on('pan', (e) => {
+      //Calculate pixel movements into 1:1 screen percents so gestures track with motion
+      let percentage = 100 / this.slideCount * e.deltaX / window.innerWidth
+      //Multiply percent by # of slide we’re on
+      let percentageCalculated = percentage - 100 / this.slideCount * this.activeSlide
+      this.swipeElLbox.style.transform = 'translateX( ' + percentageCalculated + '% )'
+      this.determineValidSwipe(e, percentage)
 
+    })
   }
+
   @Method()
-  setSlide(n) {
-    this.showSlides(this.carouselIndex = n)
+  determineValidSwipe(e, percentage){
+    if (e.isFinal) {
+      if (e.velocityX > 1) {
+        this.goTo(this.activeSlide - 1);
+      } else if (e.velocityX < -1) {
+        this.goTo(this.activeSlide + 1)
+      } else {
+        if (percentage <= -(this.sensitivity / this.slideCount))
+          this.goTo(this.activeSlide + 1);
+        else if (percentage >= (this.sensitivity / this.slideCount))
+          this.goTo(this.activeSlide - 1);
+        else
+          this.goTo(this.activeSlide);
+      }
+    }
+  }
+
+  @Method()
+  goTo(number) {
+    if (number < 0)
+      this.activeSlide = 0;
+    else if (number > this.slideCount - 1)
+      this.activeSlide = this.slideCount - 1
+    else
+      this.activeSlide = number;
+
+    // 5b. Apply transformation & smoothly animate via .is-animating CSS
+    this.swipeEL.classList.add('is-animating');
+    this.swipeElLbox.classList.add('is-animating');
+    // console.log(this.swipeEL.classList)
+    var percentage = -(100 / this.slideCount) * this.activeSlide;
+    this.swipeEL.style.transform = 'translateX( ' + percentage + '% )';
+    this.swipeElLbox.style.transform = 'translateX( ' + percentage + '% )';
+    let self = this
+    clearTimeout(this.timer);
+    this.timer = setTimeout(function () {
+      self.swipeEL.classList.remove('is-animating')
+      self.swipeElLbox.classList.remove('is-animating')
+    }, 400);
   }
 
   @Method()
   next(n) {
-    this.showSlides(this.carouselIndex += n)
+    this.goTo(this.activeSlide += n)
   }
 
   @Method()
   previous(n) {
-    this.showSlides(this.carouselIndex -= n)
+    this.goTo(this.activeSlide -= n)
   }
 
-  @Listen('pz_zoomupdate')
-  hideLightBoxButtonsend(){
-    let slides: any = document.getElementsByClassName("pinch-zoom")
-    let next: any = document.getElementsByClassName('next_fullscreen')
-    let previous: any = document.getElementsByClassName('previous_fullscreen')
-
-    let scale3D = slides[0].style.transform.split(' t', 1)
-    scale3D = scale3D[0].slice(8,-1)
-    scale3D = scale3D.split(',', 2)
-    let x: number = parseFloat(scale3D[0])
-    let y: number = parseFloat(scale3D[1])
-
-    if((0.99 <= x && x<= 1.4) && (0.99 <= y && y<= 1.4) ){
-      next[0].style.display = 'inline'
-      previous[0].style.display = 'inline'
-    }
-
-    if((0.99 > x || x> 1.01) && (0.99 > y || y> 1.01) ){
-      next[0].style.display = 'none'
-      previous[0].style.display = 'none'
-    }
-  }
-
-  @Method()
-  showSlides(n) {
-    let i: number
-    let slides: any = document.getElementsByClassName("carouselImage")
-    let dots: any = document.getElementsByClassName("dot")
-
-    if (n > slides.length) { this.carouselIndex = 1 }
-    if (n < 1) { this.carouselIndex = slides.length }
-
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"
-    }
-
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace('active', '')
-    }
-
-    slides[this.carouselIndex - 1].style.display = 'block'
-    dots[this.carouselIndex - 1].className += ' active'
-
-  }
 
   @Method()
   nextLightBox(n) {
-    this.lightBox(this.carouselIndex += n)
+    this.lightBox(this.activeSlide += n)
   }
 
   @Method()
   previousLightBox(n) {
-    this.lightBox(this.carouselIndex -= n)
+    this.lightBox(this.activeSlide -= n)
   }
 
   @Method()
   lightBox(n) {
-    this.showSlides(n)
+    this.goTo(n)
 
     let modal: any = document.getElementsByClassName('modal')
-    let slides: any = document.getElementsByClassName("pinch-zoom")
+    let wrapper: any = document.getElementsByClassName('wrapper')
     let closeButton: any = document.getElementsByClassName('close')
     let expandButton: any = document.getElementsByClassName('expand')
     let next: any = document.getElementsByClassName('next_fullscreen');
     let previous: any = document.getElementsByClassName('previous_fullscreen');
 
-
-    modal[0].style.display = 'inline'
+    modal[0].style.display = 'flex'
+    wrapper[0].style.display = 'inline'
     next[0].style.display = 'inline'
     previous[0].style.display = 'inline'
-
-    if (n > this.images.length) { this.carouselIndex = 1 }
-    if (n < 1) { this.carouselIndex = this.images.length }
-
-    slides[0].childNodes[0].src = this.images[this.carouselIndex - 1].url
-    slides[0].style.display = 'inline'
-
     closeButton[0].style.display = 'inline'
     expandButton[0].style.display = 'none'
   }
@@ -166,59 +161,66 @@ export class LumavateCarosel {
   @Method()
   closeModal() {
     let modal: any = document.getElementsByClassName('modal')
+    let wrapper: any = document.getElementsByClassName('wrapper')
     let expandButton: any = document.getElementsByClassName('expand')
     let closeButton: any = document.getElementsByClassName('close material-icons')
     let next: any = document.getElementsByClassName('next_fullscreen');
     let previous: any = document.getElementsByClassName('previous_fullscreen');
 
-
     modal[0].style.display = 'none'
+    wrapper[0].style.display = 'none'
     next[0].style.display = 'none'
     previous[0].style.display = 'none'
     closeButton[0].style.display = 'none'
     expandButton[0].style.display = 'inline'
   }
 
-
   render() {
     return (
       <div class="slideshow-container" style={{ width: "100%", height: "100%" }}>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"></meta>
-          <style>
-            @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"></meta>
+        <style>
+          @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
           </style>
+
+        <div class='container'>
           {this.images.map((item) =>
-            <div class="carouselImage" style={{ width: "100%", height: "100%" }}>
+            <div class="carouselImage" >
               <lumavate-image
                 src={item.url}
                 mode={this.mode}>
               </lumavate-image>
             </div>
           )}
+        </div>
 
-          <a class="previous" onClick={() => this.previous(1)}>&#10094;</a>
-          <a class="next" onClick={() => this.next(1)}>&#10095;</a>
+        <a class="previous" onClick={() => this.previous(1)}>&#10094;</a>
+        <a class="next" onClick={() => this.next(1)}>&#10095;</a>
 
-          <div class="dot-container">
-            {this.images.map((_, index) =>
-              <span class="dot" onClick={() => this.setSlide(index + 1)}></span>
-            )}
-          </div>
+        <div class="dot-container">
+          {this.images.map((_, index) =>
+            <span class="dot" onClick={() => this.goTo(index + 1)}></span>
+          )}
+        </div>
 
-          <a class="expand material-icons" onClick={() => this.lightBox(this.carouselIndex)}>fullscreen</a>
+        <a class="expand material-icons" onClick={() => this.lightBox(this.activeSlide)}>fullscreen</a>
 
-          <a class="previous_fullscreen" onClick={() => this.previousLightBox(1)}>&#10094;</a>
-          <a class="next_fullscreen" onClick={() => this.nextLightBox(1)}>&#10095;</a>
+        <a class="previous_fullscreen" onClick={() => this.previousLightBox(1)}>&#10094;</a>
+        <a class="next_fullscreen" onClick={() => this.nextLightBox(1)}>&#10095;</a>
 
-          <span class="close material-icons" onClick={() => this.closeModal()}>fullscreen_exit</span>
-          <div class='modal'>
-            <div class="pinch-zoom" style={{ width: "100%", height: "100%" }}>
+        <span class="close material-icons" onClick={() => this.closeModal()}>fullscreen_exit</span>
+        <div class='wrapper'>
+        </div>
+        <div class='modal'>
+          {this.images.map((item) =>
+            <div style={{ width: "100%", height: "100%" }}>
               <lumavate-image
-                src=''
+                src={item.url}
                 mode="contain" />
             </div>
-          </div >
-      </div >
-        )
-      }
+          )}
+        </div>
+      </div>
+    )
+  }
 }
