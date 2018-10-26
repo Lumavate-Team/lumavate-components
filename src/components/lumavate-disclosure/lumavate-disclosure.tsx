3b4431ac3b4652560ff612326e0b5a4ece1c3fb3
Tim Pierret <t.pierret@lumavate.com>
2018-10-26 10:11:09 -0400

    updates disclosure component

--- src/components/lumavate-disclosure/lumavate-disclosure.tsx
+++ src/components/lumavate-disclosure/lumavate-disclosure.tsx
@@ -22,11 +22,11 @@ export class LumavateDisclosure {
   @Prop() link1TextColor: string = "#4177b5";
   @Prop() link1: string = "";
 
-  @Prop() link2Text: string = "";
+  @Prop() link2Text: string = "Privacy Agreement";
   @Prop() link2TextColor: string = "#4177b5";
   @Prop() link2: string = "";
 
-  @Prop() link3Text: string = "";
+  @Prop() link3Text: string = "afsdklakhadls";
   @Prop() link3TextColor: string = "#4177b5";
   @Prop() link3: string = "";
 
@@ -37,6 +37,7 @@ export class LumavateDisclosure {
 
   getCookie(name) {
     var value = "; " + document.cookie;
+    value="";
     var parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
   }
@@ -64,12 +65,12 @@ export class LumavateDisclosure {
     else if (this.closeBox) {
       return (
         <div id="disclosureClose" style={{fontFamily: this.disclosureTextFont, backgroundColor: this.disclosureBackgroundColor, color: this.disclosureTextColor}}>
-          <div class="outside">
-            <div class="text">{this.disclosureText}</div>
-            <a id = "link1" style={{color:this.link1TextColor}} href={this.link1}>{this.link1Text}</a>
-            <a id = "link2" style={{color:this.link2TextColor}} href={this.link2}>{this.link2Text}</a>
-            <a id = "link3" style={{color:this.link3TextColor}} href={this.link3}>{this.link3Text}</a>
-            <div id="button" style={{backgroundColor: this.buttonColor, color: this.buttonTextColor, fontFamily: this.buttonTextFont}}>{this.buttonText}</div>
+          <div id="disclosureText" class="text">{this.disclosureText}</div>
+          <a id = "link1" style={{color:this.link1TextColor}} href={this.link1}>{this.link1Text}</a>
+          <a id = "link2" style={{color:this.link2TextColor}} href={this.link2}>{this.link2Text}</a>
+          <a id = "link3" style={{color:this.link3TextColor}} href={this.link3}>{this.link3Text}</a>
+          <div id="button" style={{backgroundColor: this.buttonColor, color: this.buttonTextColor, fontFamily: this.buttonTextFont}}>
+            <div id="buttonText">{this.buttonText}</div>
           </div>
         </div>
       );
@@ -77,12 +78,12 @@ export class LumavateDisclosure {
     else {
       return (
         <div id="disclosure" style={{fontFamily: this.disclosureTextFont, backgroundColor: this.disclosureBackgroundColor, color: this.disclosureTextColor}}>
-          <div class="outside">
-            <div class="text">{this.disclosureText}</div>
-            <a id = "link1" style={{color:this.link1TextColor}} href={this.link1}>{this.link1Text}</a>
-            <a id = "link2" style={{color:this.link2TextColor}} href={this.link2}>{this.link2Text}</a>
-            <a id = "link3" style={{color:this.link3TextColor}} href={this.link3}>{this.link3Text}</a>
-            <div id="button" onClick={this.close.bind(this)} style={{backgroundColor: this.buttonColor, color: this.buttonTextColor, fontFamily: this.buttonTextFont}}>{this.buttonText}</div>
+          <div id="disclosureText" class="text">{this.disclosureText}</div>
+          <a id = "link1" style={{color:this.link1TextColor}} href={this.link1}>{this.link1Text}</a>
+          <a id = "link2" style={{color:this.link2TextColor}} href={this.link2}>{this.link2Text}</a>
+          <a id = "link3" style={{color:this.link3TextColor}} href={this.link3}>{this.link3Text}</a>
+          <div id="button" onClick={this.close.bind(this)} style={{backgroundColor: this.buttonColor, color: this.buttonTextColor, fontFamily: this.buttonTextFont}}>
+            <div id="buttonText">{this.buttonText}</div>
           </div>
         </div>
       );
