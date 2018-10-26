416155028df50465f5d6c467d78652d3f2bf07dd
Tim Pierret <t.pierret@lumavate.com>
2018-10-26 10:49:40 -0400

    updated disclosure component

--- src/components/lumavate-disclosure/lumavate-disclosure.tsx
+++ src/components/lumavate-disclosure/lumavate-disclosure.tsx
@@ -37,7 +37,6 @@ export class LumavateDisclosure {
 
   getCookie(name) {
     var value = "; " + document.cookie;
-    value="";
     var parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
   }
